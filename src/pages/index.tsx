d9e763540b62c2b114055fbc72583d92d173e359
darwin808 <darwin.apolinario12@gmail.com>
2022-09-07 13:31:22 +0800

    remove update package

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,16 @@
-import axios from "axios"
+// import axios from "axios"
 import type { NextPage } from "next"
 import Head from "next/head"
-import Image from "next/image"
 import styles from "../../styles/Home.module.css"
 
-const getUser = async () => {
-   try {
-      const response = await axios.get("/api/messenger?message=hello ")
-      console.log(response)
-   } catch (error) {
-      console.error(error)
-   }
-}
+// const getUser = async () => {
+//    try {
+//       const response = await axios.get("/api/messenger?message=hello ")
+//       console.log(response)
+//    } catch (error) {
+//       console.error(error)
+//    }
+// }
 const Home: NextPage = () => {
    return (
       <div className={styles.container}>
@@ -20,7 +19,7 @@ const Home: NextPage = () => {
             <meta name="description" content="Automated Facebook Messaging 2022" />
             <link rel="icon" href="/favicon.ico" />
          </Head>
-         <button onClick={getUser}>ok</button>
+         {/* <button onClick={getUser}>ok</button> */}
          hi
       </div>
    )
